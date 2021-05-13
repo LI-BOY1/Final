892efc6a12cedac645a82d418af7f2afbdc42c78
jasperxu1233 <jasperhaojie@gmail.com>
2021-05-13 14:16:31 -0400

    Haojie

--- src/Trie2.java
+++ src/Trie2.java
@@ -155,7 +155,7 @@ public class Trie2 extends Trie{
     public List<String> searchWithPrefix(String prefix){
         return new ArrayList<>();
     }
-
+    //
     public void writeTrieToTxtfile(TrieNode2 node) throws IOException {
         comporessed_letters = new ArrayList<>();
         root_letters = new ArrayList<>();
@@ -181,26 +181,7 @@ public class Trie2 extends Trie{
 //            i++;
         }
     }
-
-//    public void encoding() throws IOException {           //method use arithmetic encoding: ((a*26 + b)*26 + c)*26 to get the integer value and turned into the binary and write into the file to comporess
-//        FileOutputStream fos = new FileOutputStream(new File("encoding.txt"));
-//        fos.write(convert(strToBinary(comporessed_letters.get(0))));  // the first thing:word length is less than 3, cannot be encoded,
-//        for(String input:comporessed_letters){                    // so it is turned into the binary string and then converted to binary number written into file
-//            if (input.length() <= 3){
-//                String[] string = input.split("");
-//                int encoding_number = 0;
-//                int sum = 0;
-//                for(String c: string){
-//                    encoding_number = (encoding_number + c.charAt(0)-'a')*26;
-//                }
-//                fos.write(encoding_number);
-//                System.out.println(encoding_number);
-//            }
-//        }
-//        fos.flush();
-//        fos.close();
-//    }
-
+      //
     public void writeBinaryToTxtfile(TrieNode2 node) throws IOException {
         turnIntoBinary(node);
         FileOutputStream fos = new FileOutputStream(new File("new_binary.dat"));
@@ -214,13 +195,6 @@ public class Trie2 extends Trie{
         }
         fos.flush();
         fos.close();
-//        BufferedWriter out = new BufferedWriter(new FileWriter("binary.dat"));
-//        out.write(String.valueOf(binary_array));
-//        for(Object key : binary_array_number){
-//            out.write(key+"\t");
-//        }
-//        out.write(binary_array_number);
-//        out.close();
     }
 
     public void turnIntoBinary(TrieNode2 node){    // method: iterate every subnode of the input node, input node included
@@ -232,25 +206,13 @@ public class Trie2 extends Trie{
     }
 
     public byte[] convert(String string) throws IOException { // turn each binary string in the binary_array into byte
-//        FileOutputStream fos = new FileOutputStream(new File("new binary.txt"));
-//        ArrayList<Byte> Byte = new ArrayList<java.lang.Byte>();
-//        for(Object c: binary_array){
         int iteration = string.length()/7;   // all the binary string in one position of the array, the length should be multiplied by 7 cause asci code determines
         byte[] arr = new byte[iteration];
         for(int i = 0 ; i < iteration; i++){    // arr contains the binary number converted from every length 7 binary string
             arr[i] = Byte.parseByte(string.substring(i*7,(i+1)*7),2);
         }
         return arr;
-//            fos.write(arr);
-//            StringBuilder string = new StringBuilder();
-//            for(Object Byte:arr){
-//                 string.append(Byte+" ");
-//            }
-//            binary_array_number.add(string);
-        }
-//        fos.close();
-//         System.out.println(binary_array.get(1).getClass());
-//    }
+    }
 
     public String strToBinary(String s)
     {
@@ -300,19 +262,10 @@ public class Trie2 extends Trie{
         return String.valueOf(a);
     }
 
-
-//    public void write(TrieNode2 node){
-//        Iterator<Map.Entry<String,TrieNode2>> entries = node.children.entrySet().iterator();
-//        while(entries.hasNext()){
-//            Map.Entry<String,TrieNode2> entry = entries.next();
-//        }
-//
-//    }
-
     public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
         Trie2 dict = new Trie2();
-//        dict.add("nihao");
+
         System.out.println(dict.word_number);
         FileReader fr = new FileReader("English dictionary.txt");
         BufferedReader br = new BufferedReader(fr);
@@ -324,44 +277,7 @@ public class Trie2 extends Trie{
         System.out.println(dict.word_number);
         System.out.println((endTime - startTime)+"ms");
 
-//        dict.write_trie();
-//
-//        System.out.println(dict.root.children.containsKey("abh"));
-
-//        FileReader fr1 = new FileReader("English dictionary.txt");
-//        BufferedReader br1 = new BufferedReader(fr1);
-//        String line1;
-//        String check;
-//        int Number = 0;
-//        while((line1 = br1.readLine()) != null){
-//            check = dict.Stringsearch(line1);
-//            if(check.equals("No")){System.out.println("have words not in the dict!");Number++;}
-//        }
-//        System.out.println(Number);
-
-
-//        Trie2 dict1 = new Trie2();
-////        dict.add("nihao");
-//        System.out.println(dict1.word_number);
-//        FileReader fr2 = new FileReader("test.txt");
-//        BufferedReader br2 = new BufferedReader(fr2);
-//        String line2;
-//        int a = 0;
-//        while((line2 = br2.readLine()) != null){
-//            dict1.add(line2);
-//            a++;
-//            if (line2.equals("aaa")){break;}
-//        }
-//        System.out.println(dict1.word_number);
-//
-////        dict1.write_trie();
-//
-//        System.out.println(dict1.root.children.containsKey("phe"));
-//        System.out.println(a);
-
         dict.writeTrieToTxtfile(dict.root);
         dict.writeBinaryToTxtfile(dict.root);
-//        dict.encoding();
-        //        System.out.println(dict.i);
     }
 }
