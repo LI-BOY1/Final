fac5e4766619cbd2ab0827533515c50b89e93b73
xiangjun meng <xiangjunmeng@xiangjuns-mbp.myfiosgateway.com>
2021-05-14 08:36:54 -0400

    update

--- src/Trie1.java
+++ src/Trie1.java
@@ -1,73 +1,75 @@
 import javax.swing.tree.TreeNode;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 
 // boyang li
 class TrieNode1{
     char ch;
     boolean word;
-    HashMap<Character, TreeNode1> children = new HashMap<>();
- //   TrieNode1[] children = new TrieNode1[26];
+    HashMap<Character, TreeNode> children = new HashMap<>();
+    TrieNode1[] children1 = new TrieNode1[26];
+	public int count;
 }
 
 // Xiangjun Meng
-public class Trie1 extends Trie{
+public abstract class Trie1 extends Trie{
     TrieNode1 root;
+    static final int[][] DIRS = {{-1, 0}, {1,0},{0,-1},{0,1}};
 
-    @Override
+	@Override
     public void add(String word){
     	if (search(word)) {
-    	    return false;
+    	    return;
         }
     	TrieNode1 cur = root;
     	for (int i = 0; i < word.length(); i++) {
-    	    TrieNode1 next = cur.children.get(word.charAt(i));
+    	    TrieNode1 next = (TrieNode1) cur.children.get(word.charAt(i));
     	    if (next == null){
     	        next = new TrieNode1();
-    	        cur.children.put(word.charAt(i), next);
+    	        cur.children.put(word.charAt(i), (TreeNode) next);
             }
     	    cur = next;
     	    cur.count++;
         }
-    	cur.isWord = true;
-    	return true;
+    	cur.word = true;
+    	return;
     }
 
     @Override
     public boolean search(String word){
         TrieNode1 cur = root;
-        for (int i = 0; i < word.length; i++) {
-            TrieNode1 next = cur.children.get(word.charAt(i));
+        for (int i = 0; i < word.length(); i++) {
+            TrieNode1 next = (TrieNode1) cur.children.get(word.charAt(i));
             if(next == null) {
                 return false;
             }
             cur = next;
         }
-        return cur.isWord;
+        return cur.word;
     }
-
+    
     @Override
-    public List<String> searchWithPrefix(TrieNode1 root, String prefix) {
-        TrieNode1 matchNode = searchNode(root, prefix);
-        List<String> result = new ArrayList<>();
-        if (matchNode == null) {
-            return result;
-        }
-        searchWithUnderNode(matchNode, new StringBuilder(prefix), result);
-        return result;
+	public boolean delete(String word) {
+    	if (!search(word)) {
+    		return false;
+    	}
+    	TrieNode1 cur = root;
+    	for (int i = 0; i < word.length(); i++) {
+    		TrieNode1 next = (TrieNode1) cur.children.get(word.charAt(i));
+    		next.count--;
+    		if(next.count == 0) {
+    			cur.children.remove(word.charAt(i));
+    			return true;
+    		}
+    		cur = next;
+    	}
+    	cur.word = false;
+    	return true;
     }
-
-    public void searchWithUnderNode (TrieNode1 current, StringBuilder curPath, List<String> result) {
-        if (current.isWord) {
-            result.add(curPath.toString());
-        }
-        for (Entry<Character, TrieNode> child : current.children.EntrySet()) {
-            curPath.append(child.getKey());
-            searchWithUnderNode(child.getValue, curPath, result);
-        }
-    }
-    
-    
 
 }
 
